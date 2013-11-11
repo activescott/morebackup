63059fe3f17a4b5a45b887f86d9b105efd939ed7
Scott Willeke <scott@willeke.com>
2013-11-10 22:39:12 -0800

    added deploy script

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+########## Links the scripts in this directory into ~/bin so they show up in the PATH and can be called anywehre.
+
+BIN=$(cd ~/bin; pwd)
+HERE=`pwd`;
+for f in $(ls *.sh)
+do
+	if [ $f != "deploy.sh" ]; then
+		#delete the existing link if it exists
+		[ -a "$BIN/$f" ] && rm -f "$BIN/$f" 
+		ln "$HERE/$f" "$BIN/$f"
+	fi
+done
